f8a08e49d1939f5c3c70ad94dc648acc8b692d52
Sascha <alexanderehret222@gmail.com>
2022-02-22 19:01:44 +0100

    improved search by tags

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import json
 class Autogallery(Thread):
     def __init__(
         self,
-        keywords: str,
         tag: str,
+        tags: str,
         path: str,
         max_pages: int = 1,
         start_page: int = 1,
@@ -18,8 +18,8 @@ class Autogallery(Thread):
     ):
         super().__init__()
         self.url = "https://yande.re/post.json?api_version=2&limit=1000"
-        self.keywords = keywords.split(" | ")
         self.tag = tag
+        self.tags = tags.split(" | ")
         self.path = path
         self.max_pages = max_pages
         self.start_page = start_page
@@ -27,34 +27,20 @@ class Autogallery(Thread):
 
     def request(self) -> list:
         filtered = []
-        print(f'Searching for {", ".join(self.keywords)}...')
+        print(f'Searching for {", ".join(self.tags)}...')
+        self.url += f"&tags={'+'.join(self.tags)}"  # + is used to search for multiple tags. params will only convert it to %2B
 
         for i in range(self.max_pages):
             try:
                 response = get(
-                    self.url, params={"tags": self.tag, "page": self.start_page + i}
+                    self.url,
+                    params={"tag": self.tag, "page": self.start_page + i},
                 )
                 res = response.json()
-                _filteredByTag = list(
-                    filter(
-                        lambda post: self.tag in post.get("tags").lower(),
-                        res.get("posts"),
-                    )
-                )
-                _filteredByKeywords = list(
-                    filter(
-                        lambda post: [
-                            keyword
-                            for keyword in self.keywords
-                            if keyword.lower() in post.get("tags").lower()
-                        ]
-                        != [],
-                        _filteredByTag,
-                    )
-                )
-                filtered.extend(_filteredByKeywords)
+                posts = res.get("posts")
+                filtered.extend(posts)
                 print(
-                    f"..extend {len(_filteredByKeywords)} posts. {len(filtered)} total at page {self.start_page + i}."
+                    f"..extend {len(posts)} posts. {len(filtered)} total at page {self.start_page + i}."
                 )
             except json.JSONDecodeError:
                 if "can't go beyond requested page" in response.text.lower():
@@ -110,10 +96,10 @@ class Autogallery(Thread):
 
 def main():
     while 1:
-        keywords = input(
-            '\nKeywords (words that must be included in tags, seperate with |. Example: "some | text" ): '
+        tag = input("Tag (fixed tag to search for, use _ for spaces): ")
+        tags = input(
+            'Tags (tags to search for, seperate with |. Example: "tag1 | tag2 | tag3"): '
         )
-        tag = input("Tag (actual tag to search for): ")
         path = input("Save path (path the images get saved at): ")
         max_pages = int(input("Max pages (max number of pages to search): "))
         start_page = int(input("Start page (page to start at): "))
@@ -124,7 +110,7 @@ def main():
             \r): """
         )
         autogal_thread = Autogallery(
-            keywords, tag, path, max_pages, start_page, install_mode
+            tag, tags, path, max_pages, start_page, install_mode
         )
         autogal_thread.start()
         while autogal_thread.is_alive():
